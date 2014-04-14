a637c32096e12b757a9b895f24b875e8f5e904d1
Brian Williams <brian@mxapps.com>
2014-04-14 11:57:10 -0500

    Updated Stats Doc format.

--- stats_doc.py
+++ stats_doc.py
@@ -5,20 +5,21 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy import schema, types, Table
 from datetime import datetime
 from operator import attrgetter
+from mixcoatl.admin.account import Account
 from mixcoatl.infrastructure.server import Server
 from mixcoatl.settings.load_settings import settings as mixcoatl_settings
 
 if os.environ['SQL_USER'] is not None:
 	metadata = schema.MetaData()
 	sql_creds = os.environ['SQL_USER']
-	
+
 	if os.environ['SQL_PASSWORD'] is not None:
 		sql_creds += ":"+os.environ['SQL_PASSWORD']
 
 	engine = create_engine('mysql://'+sql_creds+'@127.0.0.1/poc')
 	metadata.bind = engine
 	conn = engine.connect()
-	
+
 	poc = Table('poc', metadata, autoload=True)
 	s = poc.select().where(poc.c.active == 'Y' and poc.c.trial_start is not None)
 	rs = s.execute()
@@ -39,49 +40,49 @@ if os.environ['SQL_USER'] is not None:
 		mixcoatl_settings.set_api_version('2013-12-07')
 		mixcoatl_settings.set_access_key(row['api_key'])
 		mixcoatl_settings.set_secret_key(row['secret_key'])
-	
-		active_only = 'false'
-		servers = sorted(Server.all(active_only), key=attrgetter('start_date'))
-		running = 0
-		launches_count = 0
-		launches = dict()
 
-		for server in servers:
-			datekey = datetime.strptime(server.start_date, '%Y-%m-%dT%H:%M:%S.%f+0000').strftime("%Y-%m-%d")
-			trial_start_formatted = datetime.strptime(str(row['trial_start']), '%Y-%m-%d 00:00:00').strftime("%Y-%m-%d")
-			
-			if datekey > trial_start_formatted:
-				launches_count += 1
-	
-				if datekey in launches:
-					launches[datekey] += 1
-				else:
-					launches[datekey] = 1
-		
-				if server.status != 'TERMINATED':
-					running += 1
-	
+		for account in Account.all():
+			params = {'activeOnly':'false', 'accountId':account.account_id}
+			servers = sorted(Server.all(params=params), key=attrgetter('start_date'))
+			running = 0
+			launches_count = 0
+			launches = dict()
 
-		if row['trial_start'] is not None:
-			trial_start_formatted = datetime.strptime(str(row['trial_start']), '%Y-%m-%d 00:00:00').strftime("%Y-%m-%d")
-		else:
-			trial_start_formatted = "N/A"
+			for server in servers:
+				datekey = datetime.strptime(server.start_date, '%Y-%m-%dT%H:%M:%S.%f+0000').strftime("%Y-%m-%d")
+				trial_start_formatted = datetime.strptime(str(row['trial_start']), '%Y-%m-%d 00:00:00').strftime("%Y-%m-%d")
 
-		if row['trial_end'] is not None:
-			trial_end_formatted = datetime.strptime(str(row['trial_end']), '%Y-%m-%d 00:00:00').strftime("%Y-%m-%d")
-		else:
-			trial_end_formatted = "N/A"
+				if datekey > trial_start_formatted:
+					launches_count += 1
 
-		msg += "\t<tr>\n"
-		msg += "\t\t<td>"+str(row['client_name'])+"</td>\n"
-		msg += "\t\t<td>"+str(row['se_name'])+"</td>\n"
-		msg += "\t\t<td>"+str(row['ae_name'])+"</td>\n"
-		#msg += "\t\t<td>Test</td>\n"
-		msg += "\t\t<td>"+str(trial_start_formatted)+"</td>\n"
-		msg += "\t\t<td>"+str(trial_end_formatted)+"</td>\n"
-		msg += "\t\t<td>"+str(running)+"</td>\n"
-		msg += "\t\t<td>"+str(launches_count)+"</td>\n"
-		msg += "\t</tr>\n"
+					if datekey in launches:
+						launches[datekey] += 1
+					else:
+						launches[datekey] = 1
+
+					if server.status != 'TERMINATED':
+						running += 1
+
+			if row['trial_start'] is not None:
+				trial_start_formatted = datetime.strptime(str(row['trial_start']), '%Y-%m-%d 00:00:00').strftime("%Y-%m-%d")
+			else:
+				trial_start_formatted = "N/A"
+
+			if row['trial_end'] is not None:
+				trial_end_formatted = datetime.strptime(str(row['trial_end']), '%Y-%m-%d 00:00:00').strftime("%Y-%m-%d")
+			else:
+				trial_end_formatted = "N/A"
+
+			msg += "\t<tr>\n"
+			msg += "\t\t<td>"+str(row['client_name'])+"</td>\n"
+			msg += "\t\t<td>"+str(row['se_name'])+"</td>\n"
+			msg += "\t\t<td>"+str(row['ae_name'])+"</td>\n"
+			#msg += "\t\t<td>Test</td>\n"
+			msg += "\t\t<td>"+str(trial_start_formatted)+"</td>\n"
+			msg += "\t\t<td>"+str(trial_end_formatted)+"</td>\n"
+			msg += "\t\t<td>"+str(running)+"</td>\n"
+			msg += "\t\t<td>"+str(launches_count)+"</td>\n"
+			msg += "\t</tr>\n"
 
 msg += "</table>"
 
